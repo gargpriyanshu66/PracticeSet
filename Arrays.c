29d48d3e58ddb3ed558404e89d8d72f37dedc95d
gargpriyanshu66 <ishuvivoy53@gmail.com>
2023-05-23 12:55:57 +0530

    C programming

--- Arrays.c
+++ Arrays.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 int main(){
-    int marks1 = 98;
-    int marks2 = 98;
-    int marks3= 98;
+    int marks1 = 100;
+    int marks2 = 100;
+    int marks3= 100;
 
     int marks[] = {98,99,87};
     return 0;
